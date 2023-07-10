7ff5cec8027604bac2635812e9a683294a277062
Dennis Schmidt <129665818+DS-178@users.noreply.github.com>
2023-07-10 19:12:26 +0200

    Add files via upload

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'SLRP MENU DEFAULT'
+
+client_scripts {
+	'@es_extended/client/wrapper.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf'
+}
+
+dependencies {
+	'es_extended'
+}
+
+exports {
+	'OpenMenu'
+}
+
+
+
